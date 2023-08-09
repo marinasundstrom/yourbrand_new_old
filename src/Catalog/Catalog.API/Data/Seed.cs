3abb5e8526e39069995063c5f96453ae50c22b66
Marina Sundström <marna.li@outlook.com>
2023-08-09 13:35:50 +0200

    Update seed

--- src/Catalog/Catalog.API/Data/Seed.cs
+++ src/Catalog/Catalog.API/Data/Seed.cs
@@ -20,6 +20,54 @@ public static class Seed
                 Price = 10,
                 RegularPrice = null,
                 Image = "http://127.0.0.1:10000/devstoreaccount1/images/products/biscotti.jpeg"
+            },
+            new Product() {
+                Id = "brewed-coffee",
+                Name = "Signature Brew",
+                Description = "Freshly brewed coffee",
+                Price = 32,
+                RegularPrice = null,
+                Image = "http://127.0.0.1:10000/devstoreaccount1/images/products/coffee.jpeg"
+            },
+            new Product() {
+                Id = "caffe-latte",
+                Name = "Caffe Latte",
+                Description = "Freshly ground espresso coffee with steamed milk",
+                Price = 32,
+                RegularPrice = 42,
+                Image = "http://127.0.0.1:10000/devstoreaccount1/images/products/caffe-latte.jpeg"
+            },
+            new Product() {
+                Id = "cinnamon-roll",
+                Name = "Cinnamon roll",
+                Description = "Newly baked cinnamon rolls",
+                Price = 22,
+                RegularPrice = null,
+                Image = "http://127.0.0.1:10000/devstoreaccount1/images/products/cinnamon-roll.jpeg"
+            },
+            new Product() {
+                Id = "espresso",
+                Name = "Espresso",
+                Description = "Single shot espresso",
+                Price = 32,
+                RegularPrice = null,
+                Image = "http://127.0.0.1:10000/devstoreaccount1/images/products/espresso.jpeg"
+            },
+            new Product() {
+                Id = "milkshake",
+                Name = "Milkshake",
+                Description = "Our fabulous milkshake",
+                Price = 52,
+                RegularPrice = null,
+                Image = "http://127.0.0.1:10000/devstoreaccount1/images/products/milkshake.jpeg"
+            },
+            new Product() {
+                Id = "mocca-latte",
+                Name = "Mocca Latte",
+                Description = "Caffe Latte with chocolate syrup",
+                Price = 32,
+                RegularPrice = null,
+                Image = "http://127.0.0.1:10000/devstoreaccount1/images/products/mocca-latte.jpeg"
             }
         );
 
