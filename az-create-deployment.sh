99427cf60730644b3578b13a35355395f537d158
Marina Sundström <marna.li@outlook.com>
2023-10-03 12:27:40 +0200

    Update Bicep files

--- az-create-deployment.sh
+++ az-create-deployment.sh
@@ -1,13 +1,8 @@
 #!sh
 
-$RESOURCE_GROUP="YourBrand"
-$LOCATION="swedencentral"
+RESOURCE_GROUP="YourBrand2"
+LOCATION="swedencentral"
 
-az group create `
---name $RESOURCE_GROUP `
---location $LOCATION
+az group create --name $RESOURCE_GROUP --location $LOCATION
 
-az deployment group create `
---resource-group $RESOURCE_GROUP `
---template-file "./bicep/main.bicep" `
---parameters "./bicep/main.parameters.json"
+az deployment group create --resource-group $RESOURCE_GROUP --template-file "./bicep/main.bicep" --parameters "./bicep/main.parameters.json"
